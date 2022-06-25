c3c405d020e2fbf3407a0931db97ebbd26148b05
N/A <N/A>
2022-06-25 19:50:40 +0300

    Updated edition, README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
+//! # minihandle
+//!
 //! Simple Rust crate for managing handles, a.k.a. weak references, a.k.a. generational indices;
 //! as well as creating pools of values referred to by such handles.
 //!
 //! See [http://bitsquid.blogspot.com/2014/08/building-data-oriented-entity-system.html](http://bitsquid.blogspot.com/2014/08/building-data-oriented-entity-system.html)
+//!
+//! Dependencies:
+//!
+//! - [`static_assertions`](https://crates.io/crates/static_assertions)
+//! - [`num-traits`](https://crates.io/crates/num-traits (optional, used by the `"index"` feature))
+//! - path dependency on [`miniunchecked`](https://github.com/alex05447/miniunchecked) (TODO: Github dependency?)
 
 mod handle_manager;
 mod util;
